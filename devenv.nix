a10da84a5139f1e6b08f686e68a4c21bdad04dca
lethusang <kotjomelalethusang@gmail.com>
2026-09-01 13:41:26 +0200

    cleaned up devenv

--- devenv.nix
+++ devenv.nix
@@ -7,50 +7,26 @@
 }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
   packages = [
     pkgs.delve
     pkgs.golangci-lint
     pkgs.git
   ];
 
-  # https://devenv.sh/languages/
-  languages.go.enable = true;
+  languages.go = {
+    enable = true;
+    version = "1.27.0";
+    enableHardeningWorkaround = true;
+    delve.enable = true;
+    lsp.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  # scripts.hello.exec = ''
-  #   echo hello from $GREET
-  # '';
-
-  # https://devenv.sh/basics/
-  # enterShell = ''
-  #   hello         # Run scripts directly
-  #   git --version # Use packages
-  # '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  # enterTest = ''
-  #   echo "Running tests"
-  #   git --version | grep --color=auto "${pkgs.git.version}"
-  # '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  };
+  overlays = [
+    (final: prev: {
+      go-tools = prev.go-tools.overrideAttrs (old: {
+        doCheck = false;
+      });
+    })
+  ];
 }
